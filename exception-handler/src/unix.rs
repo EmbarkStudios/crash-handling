f76a5ab3aadc6207fa0ae9fcda448cc115a3a3b0
Jake Shadle <jake.shadle@embark-studios.com>
2021-12-09 08:57:05 +0100

    Require static string
    This function is only intended to be called in a compromised context,
    which means it should not be doing heap allocations

--- exception-handler/src/unix.rs
+++ exception-handler/src/unix.rs
@@ -14,7 +14,7 @@ macro_rules! debug_print {
 
 /// Writes the specified string directly to stderr. This is safe to be called
 /// from within a compromised context.
-pub fn write_stderr(s: &str) {
+pub fn write_stderr(s: &'static str) {
     unsafe {
         libc::write(2, s.as_ptr().cast(), s.len());
     }
