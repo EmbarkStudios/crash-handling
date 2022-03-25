508c431805bcd66b9e9d4649b29992af28dc2a8f
Jake Shadle <jake.shadle@embark-studios.com>
2022-03-25 10:21:00 +0100

    Yay consistency!

--- exception-handler/tests/segv.rs
+++ exception-handler/tests/segv.rs
@@ -3,16 +3,15 @@ mod shared;
 #[test]
 fn handles_segv() {
     #[cfg(unix)]
-    {
-        shared::handles_signal(shared::Signal::Segv, sadness_generator::raise_segfault);
-        // TODO: For some reason this hangs in the std::sync::mpsc::blocking::WaitToken::wait_max_until
-        // so we just cheat since this is the only test in this binary, but should
-        // be looked at
-        std::process::exit(0);
-    }
+    shared::handles_signal(shared::Signal::Segv, sadness_generator::raise_segfault);
     #[cfg(windows)]
     shared::handles_exception(
         shared::ExceptionCode::Segv,
         sadness_generator::raise_segfault,
     );
+
+    // TODO: For some reason this hangs in the std::sync::mpsc::blocking::WaitToken::wait_max_until
+    // so we just cheat since this is the only test in this binary, but should
+    // be looked at, but hey, consistent on windows and unix so that's fun!
+    std::process::exit(0);
 }
