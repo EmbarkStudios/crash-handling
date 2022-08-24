60f73606aab0075ed54be77ddf6561aba426259c
Sebastian Zivota <loewenheim@users.noreply.github.com>
2022-08-24 09:49:33 +0200

    diskwrite example: make directory handling more robust (#55)
    * diskwrite example: make directory handling more robust
    
    * Cleanup
    
    Co-authored-by: Jake Shadle <jake.shadle@embark-studios.com>

--- minidumper/examples/diskwrite.rs
+++ minidumper/examples/diskwrite.rs
@@ -18,8 +18,15 @@ fn main() {
             ) -> Result<(std::fs::File, std::path::PathBuf), std::io::Error> {
                 let uuid = uuid::Uuid::new_v4();
 
-                let pb = std::path::PathBuf::from(format!("dumps/{}.dmp", uuid));
-                Ok((std::fs::File::create(&pb)?, pb))
+                let dump_path = std::path::PathBuf::from(format!("dumps/{uuid}.dmp"));
+
+                if let Some(dir) = dump_path.parent() {
+                    if !dir.try_exists()? {
+                        std::fs::create_dir_all(dir)?;
+                    }
+                }
+                let file = std::fs::File::create(&dump_path)?;
+                Ok((file, dump_path))
             }
 
             /// Called when a crash has been fully written as a minidump to the provided
@@ -45,8 +52,7 @@ fn main() {
 
             fn on_message(&self, kind: u32, buffer: Vec<u8>) {
                 log::info!(
-                    "kind: {}, message: {}",
-                    kind,
+                    "kind: {kind}, message: {}",
                     String::from_utf8(buffer).unwrap()
                 );
             }
