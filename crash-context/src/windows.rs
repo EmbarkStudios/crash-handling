d320f04dcfdd4e38378378fb8e6e206214701475
Jake Shadle <jake.shadle@embark-studios.com>
2022-11-17 11:53:26 +0100

    Cleanup implementation, add aarch64

--- crash-context/src/windows.rs
+++ crash-context/src/windows.rs
@@ -3,15 +3,12 @@ pub struct CrashContext {
     /// The information on the exception.
     ///
     /// Note that this is a pointer into the actual memory of the crashed process,
-    /// and is a pointer to an [EXCEPTION_POINTERS](https://docs.rs/windows-sys/0.35.0/windows_sys/Win32/System/Diagnostics/Debug/struct.EXCEPTION_POINTERS.html)
-    ///
-    /// We intentionally don't use `windows-sys` here as the type information is
-    /// completely lost when crossing process boundaries anyways
-    pub exception_pointers: *const std::ffi::c_void,
+    /// and is a pointer to an [EXCEPTION_POINTERS](https://learn.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-exception_pointers)
+    pub exception_pointers: *const ffi::EXCEPTION_POINTERS,
     /// The top level exception code from the exception_pointers. This is provided
     /// so that external processes don't need to use `ReadProcessMemory` to inspect
     /// the exception code
-    pub exception_code: i32,
+    pub exception_code: u32,
     /// The pid of the process that crashed
     pub process_id: u32,
     /// The thread id on which the exception occurred
